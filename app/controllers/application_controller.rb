2c81603ebed777931591214825e1cc7c1810b73b
jmoypace <jmoypace@gmail.com>
2019-06-30 03:08:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,9 +40,9 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect "/account"
+      redirect to "/account"
     else
-      redirect "/failure"
+      redirect to "/failure"
     end
   end
 
